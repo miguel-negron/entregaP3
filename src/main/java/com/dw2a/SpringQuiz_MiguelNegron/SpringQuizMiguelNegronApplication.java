7c966717af901d18db662a2c9e95a5532192caee
miguel <miguel.negron@gmail.com>
2020-10-22 19:37:31 +0100

    primer commit quiz con plantillas copiadas

---
+++ src/main/java/com/dw2a/SpringQuiz_MiguelNegron/SpringQuizMiguelNegronApplication.java
@@ -0,0 +1,13 @@
+package com.dw2a.SpringQuiz_MiguelNegron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuizMiguelNegronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuizMiguelNegronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dw2a/SpringQuiz_MiguelNegron/SpringQuizMiguelNegronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw2a.SpringQuiz_MiguelNegron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuizMiguelNegronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
