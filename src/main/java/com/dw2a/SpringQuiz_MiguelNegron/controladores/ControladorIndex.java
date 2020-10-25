b62b4e858de9ebb214244c5a46b39e083cc4f121
miguel <miguel.negron@gmail.com>
2020-10-25 11:50:44 +0100

    copiado ejemplo de session y added parametros a paginador

--- src/main/java/com/dw2a/SpringQuiz_MiguelNegron/controladores/ControladorIndex.java
+++ src/main/java/com/dw2a/SpringQuiz_MiguelNegron/controladores/ControladorIndex.java
@@ -1,13 +1,69 @@
 package com.dw2a.SpringQuiz_MiguelNegron.controladores;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorIndex {
 
-	@RequestMapping("")
+	@RequestMapping("/session")
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("")
+	public String process(Model model, HttpSession session) {
+		@SuppressWarnings("unchecked")
+		List<String> respuestas = (List<String>) session.getAttribute("RESPUESTAS");
+		if (respuestas == null) {
+			respuestas = new ArrayList<>();
+		}
+
+		model.addAttribute("sessionRespuestas", respuestas);
+		return "index";
+	}
+
+	@PostMapping("/persistMessage")
+	public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
+		@SuppressWarnings("unchecked")
+		List<String> respuestas = (List<String>) request.getSession().getAttribute("RESPUESTAS");
+		if (respuestas == null) {
+			respuestas = new ArrayList<>();
+			request.getSession().setAttribute("RESPUESTAS", respuestas);
+		}
+
+		respuestas.add(msg);
+		request.getSession().setAttribute("RESPUESTAS", respuestas);
+		return "redirect:/session";
+	}
+
+	@PostMapping("/enviarRespuesta")
+	public String enviarRespuesta(@RequestParam("msg") String msg, HttpServletRequest request) {
+		@SuppressWarnings("unchecked")
+		List<String> respuestas = (List<String>) request.getSession().getAttribute("RESPUESTAS");
+		if (respuestas == null) {
+			respuestas = new ArrayList<>();
+			request.getSession().setAttribute("RESPUESTAS", respuestas);
+		}
+
+		respuestas.add(msg);
+		request.getSession().setAttribute("RESPUESTAS", respuestas);
+		return "redirect:/session";
+	}
+
+	@PostMapping("/destroy")
+	public String destroySession(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return "redirect:/session";
 	}
 }
